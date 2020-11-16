3f1451132ba6d2c218c1d65a74b70077b7730aa9
xielidong <2047355787@qq.com>
2020-11-16 11:15:17 +0800

    This is the second version of exercise 3-1,delete try and except.

--- ex_03_01.py
+++ ex_03_01.py
@@ -1,11 +1,9 @@
 hour=input("Enter hour:")
 money=input("Enter money:")
-try:
-    h=(float)(hour)
-    m=(float)(money)
-    if h<=40 :
-        print(h*m)
-    else:
-        print(40*m+(h-40)*1.5*m)
-except:
-    print("Enter data error")
+
+h=(float)(hour)
+m=(float)(money)
+if h<=40 :
+     print(h*m)
+else:
+    print(40*m+(h-40)*1.5*m)
